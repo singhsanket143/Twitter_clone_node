058141990eb18e62df8c301759f20e916e847d0c
singhsanket143 <singhsanket143@gmail.com>
2021-08-16 13:56:57 +0530

    Added partials and layouts

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = require('./src/routes/index');
+
+var expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 
-app.use('/', router);
+
+app.use(expressLayouts);
+app.set('layout', __dirname+'/src/views/layouts/layout');
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
+app.use('/', router);
+// app.set('layout', true);
+
+
 
 app.listen(3000, () => {
     console.log('Server started at 3000!!');
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,3 @@
 module.exports.profile = function(req, res) {
-    return res.render('users/user_profile');
+    return res.render('users/user_profile', {layout: __dirname+'/../../src/views/layouts/user_layout'});
 }
