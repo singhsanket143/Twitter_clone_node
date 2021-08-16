4d95585f00d489cb0ad2258a54bb5ca95437cd1f
singhsanket143 <singhsanket143@gmail.com>
2021-08-16 14:10:45 +0530

    Added static assets

--- index.js
+++ index.js
@@ -4,16 +4,20 @@ const router = require('./src/routes/index');
 var expressLayouts = require('express-ejs-layouts');
 
 const app = express();
-
+app.use(express.static(__dirname+'/src/assets'));
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use(expressLayouts);
 app.set('layout', __dirname+'/src/views/layouts/layout');
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
+
+
 app.use('/', router);
 // app.set('layout', true);
 
-
+console.log(__dirname+'/src/assets');
 
 app.listen(3000, () => {
     console.log('Server started at 3000!!');
