6fa677d1ac26d354cb5ff4020ed112195fba30ae
singhsanket143 <singhsanket143@gmail.com>
2021-08-17 21:35:27 +0530

    Added functionality that if user is already signed in then dont go to signin or signup

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,12 +5,18 @@ const profile = function(req, res) {
 }
 
 const signUp = function(req, res) {
+    if(req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
     return res.render('users/user_sign_up', {
         title: 'Twitter | Sign Up'
     });
 }
 
 const signIn = function(req, res) {
+    if(req.isAuthenticated()) {
+        return res.redirect('/users/profile');
+    }
     return res.render('users/user_sign_in', {
         title: 'Twitter | Sign In'
     });
