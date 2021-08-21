a6d9cfbc7798e2ad96345a143a1f796f603501ef
singhsanket143 <singhsanket143@gmail.com>
2021-08-21 20:52:15 +0530

    Delete added for tweets

--- src/controllers/tweetController.js
+++ src/controllers/tweetController.js
@@ -1,5 +1,5 @@
 const Tweet = require('../models/tweet');
-
+const Comment = require('../models/comment');
 const create = function(req, res) {
     Tweet.create({
         content: req.body.content,
@@ -13,4 +13,20 @@ const create = function(req, res) {
     })
 }
 
-module.exports = {create};
+const destroy = function(req, res) {
+    console.log("Hit");
+    Tweet.findById(req.params.id, function(err, tweet) {
+        if(err) {
+            return res.redirect('/');
+        }
+        if(tweet.user == req.user.id) {
+            tweet.remove();
+            Comment.deleteMany({tweet: req.params.id}, function(err) {
+                return res.redirect('back');
+            });
+        } else {
+            return res.redirect('back');
+        }
+    })
+}
+module.exports = {create, destroy};
--- src/routes/tweet.js
+++ src/routes/tweet.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const passport = require('passport');
 
-const {create} = require('../controllers/tweetController');
+const {create, destroy} = require('../controllers/tweetController');
 
 const router = express.Router();
 
 router.post('/create', passport.checkAuthentication,create);
+router.get('/destroy/:id', passport.checkAuthentication, destroy);
 
 module.exports = router;
